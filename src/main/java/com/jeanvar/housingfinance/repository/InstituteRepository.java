729198a7de12e4151ac1855f43709b07706446b6
Taeksoon Jang <taeksoon.jang@linecorp.com>
2019-04-13 21:31:15 +0900

    Fixed bug: changed method name to InstituteRepository.findByCode() to work with changed field name 'code' in Institute.

--- src/main/java/com/jeanvar/housingfinance/repository/InstituteRepository.java
+++ src/main/java/com/jeanvar/housingfinance/repository/InstituteRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface InstituteRepository extends JpaRepository<Institute, Long> {
-    Optional<Institute> findByInstituteCode(String code);
+    Optional<Institute> findByCode(String code);
 }
