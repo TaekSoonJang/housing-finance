98c45e93ba8c6ac9f45884c46c67d87e46911e10
Taeksoon Jang <taeksoon.jang@linecorp.com>
2019-04-13 19:10:52 +0900

    Initial commit.

---
+++ src/main/java/com/jeanvar/housingfinance/HousingFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.jeanvar.housingfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingFinanceApplication.class, args);
+	}
+
+}
